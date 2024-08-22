132f4a5fa3fc667cf01114285409a2d9688cc5d2
Rajat Tiwari <rajattiwari1099@gmail.com>
2024-08-22 18:10:31 -0500

    Create selected-plays.py

---
+++ selected-plays.py
@@ -0,0 +1,42 @@
+
+import discord
+from discord.ext import commands
+from discord.utils import get
+import youtube_dl
+import os
+import shutil 
+import time
+from os import system
+import aiohttp
+import random
+
+TOKEN = 'YOUR_BOT_TOKEN_HERE' # Bot Tokken
+BOT_PREFIX = '-' # Bot prefix
+
+bot = commands.Bot(command_prefix = BOT_PREFIX)
+bot.remove_command('help') 
+
+
+@bot.command()
+async def reload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
+    print("Reloaded : " + str(extension))
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+
+@bot.event
+async def on_ready():
+    '''
+    Prints Logged in when the Bot is Ready
+    '''
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('-help for Cookies'))
+    print('Logged in as : ' + bot.user.name + '\n')
+
+
+bot.run(TOKEN)
